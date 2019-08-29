d69a12a8db076de51de319aa5aaf84cdc79da2fb
Arijit Saha <arijitbiley@gmail.com>
2019-08-30 00:04:01 +0530

    Transaction App

--- rewards_engine/transaction_app/forms.py
+++ rewards_engine/transaction_app/forms.py
@@ -5,4 +5,18 @@ from transaction_app.models import Transaction
 class TransactionCreateForm(forms.ModelForm):
     class Meta:
         model=Transaction
-        fields="__all__"
+        fields=['id','value','time_stamp','user_name']
+        widgets =   {
+            'id'    :   forms.TextInput(attrs={
+                'placeholder'   :   'Give ID',
+                'disabled'      :   True,
+            }),
+        }
+
+    def __init__(self,*args,**kwargs):
+        super(TransactionCreateForm,self).__init__(*args,**kwargs)
+        for visible in self.visible_fields():
+            if 'class' in visible.field.widget.attrs:
+                visible.field.widget.attrs['class']+=' form-control'
+            else:
+                visible.field.widget.attrs['class']='form-control'
